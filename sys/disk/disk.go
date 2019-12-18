712aa142f14c6393833b3b0454518f1094a5d74a
郭朝辉 <guochaohui@phoenixtv.com>
2019-12-18 17:36:09 +0800

    dis

--- sys/disk/disk.go
+++ sys/disk/disk.go
@@ -4,6 +4,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"syscall"
 )
 
 type DiskInfo struct {
@@ -66,4 +67,23 @@ func deleteExtraSpace(s string) string {
 		spc_index = reg.FindStringIndex(string(s2))
 	}
 	return string(s2)
+}
+
+type DiskStatus struct {
+	All  uint64 `json:"all"`
+	Used uint64 `json:"used"`
+	Free uint64 `json:"free"`
+}
+
+// disk usage of path/disk
+func DiskUsage(path string) (disk DiskStatus) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		return
+	}
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+	return
 }
